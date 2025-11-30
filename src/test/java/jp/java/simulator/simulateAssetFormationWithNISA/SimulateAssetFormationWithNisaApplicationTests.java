bf74065fafe8af18af0d0d54d9d00f90e2744357
a8122011 <axxdzn412@gmail.com>
2025-11-30 20:18:49 +0900

    Update SimulateAssetFormationWithNisaApplicationTests.java

--- src/test/java/jp/java/simulator/simulateAssetFormationWithNISA/SimulateAssetFormationWithNisaApplicationTests.java
+++ src/test/java/jp/java/simulator/simulateAssetFormationWithNISA/SimulateAssetFormationWithNisaApplicationTests.java
@@ -1,1 +1,13 @@
+package jp.java.simulator.simulateAssetFormationWithNISA;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulateAssetFormationWithNisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
