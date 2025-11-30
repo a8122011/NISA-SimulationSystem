cb96a4edb7f0a120a0fd2c712f83cb6796a5eace
a8122011 <axxdzn412@gmail.com>
2025-12-01 00:49:44 +0900

    Update HomeController.java

--- src/main/java/jp/java/simulator/simulateAssetFormationWithNISA/HomeController.java
+++ src/main/java/jp/java/simulator/simulateAssetFormationWithNISA/HomeController.java
@@ -16,7 +16,7 @@ import java.util.*;
 @Controller
 public class HomeController {
                           
-    record LifeEventsParams(
+    public static record LifeEventsParams(
       String lifeEvent1, int lifeEventAge1, double requiredFunds1, 
       String lifeEvent2, int lifeEventAge2, double requiredFunds2, 
       String lifeEvent3, int lifeEventAge3, double requiredFunds3, 
